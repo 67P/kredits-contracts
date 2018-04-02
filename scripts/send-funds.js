8301514c6442d64ed2e59bbe86dc113eb513c3e9
bumi <hello@michaelbumann.com>
2018-04-02 16:53:25 +0200

    Add script to send funds from the main account
    Helpful in development mode using ganache to fund another account
    
    usage:
    
    truffle exec scripts/send-funds.js <address> <amount in ether>

---
+++ scripts/send-funds.js
@@ -0,0 +1,11 @@
+
+module.exports = function(cb) {
+  let recipient = process.argv[4];
+  if (!recipient) {
+    console.log('Please provide a recipient address');
+    process.exit();
+  }
+  let amount = parseInt(process.argv[5]) || 1;
+  console.log(recipient);
+  web3.eth.sendTransaction({to: recipient, value: web3.toWei(amount), from: web3.eth.accounts[0]}, console.log);
+}
