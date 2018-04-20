2503ac7c73774ae97d8629294732ac7b2f4feee2
bumi <hello@michaelbumann.com>
2018-04-20 12:56:09 +0200

    Refactor helper scripts to use kredits module
    This reuses the kredits library functions in the helper scripts and
    seeds. We no longer need to add IPFS hashes manually but simply can
    provider contributor/proposal data.

--- config/seeds.js
+++ config/seeds.js
@@ -1,65 +1,9 @@
-let contractCalls = {
-  Contributors: {
-    addContributor: [
-      // make sure to use an IPFS hash of one of the objects in ipfsContent
-      ['0x24dd2aedd8a9fe52ac071b3a23b2fc8f225c185e', '0x272bbfc66166f26cae9c9b96b7f9590e095f02edf342ac2dd71e1667a12116ca', 18, 32, true], // QmQyZJT9uikzDYTZLhhyVZ5ReZVCoMucYzyvDokDJsijhj
-      ['0xa502eb4021f3b9ab62f75b57a94e1cfbf81fd827', '0x9569ed44826286597982e40bbdff919c6b7752e29d13250efca452644e6b4b25', 18, 32, true] // QmYPu8zvtfDy18ZqHCviVxnKtxycw5UTJLJyk9oAEjWfnL
-    ]
-  },
-  Operator: {
-    addProposal: [
-      [2, 23, '0x1e1a168d736fc825213144973a8fd5b3cc9f37ad821a8b3d9c3488034bbf69d8', 18, 32], // QmQNA1hhVyL1Vm6HiRxXe9xmc6LUMBDyiNMVgsjThtyevs"
-      [3, 42, '0x1e1a168d736fc825213144973a8fd5b3cc9f37ad821a8b3d9c3488034bbf69d8', 18, 32], // QmQNA1hhVyL1Vm6HiRxXe9xmc6LUMBDyiNMVgsjThtyevs"
-      [3, 100, '0x1e1a168d736fc825213144973a8fd5b3cc9f37ad821a8b3d9c3488034bbf69d8', 18, 32] // QmQNA1hhVyL1Vm6HiRxXe9xmc6LUMBDyiNMVgsjThtyevs"
-    ],
-    vote: [
-      [1]
-    ]
-  }
-};
-let ipfsContent = [
-  {
-    "@context": "https://schema.kosmos.org",
-    "@type": "Contributor",
-    "kind": "person",
-    "name": "Râu Cao",
-    "url": "https://sebastian.kip.pe",
-    "accounts": [
-      {
-        "site": "github.com",
-        "username": "skddc",
-        "uid": 842,
-        "url": "https://github.com/skddc/"
-      },
-    ]
-  },
-  {
-    "@context": "https://schema.kosmos.org",
-    "@type": "Contributor",
-    "kind": "person",
-    "name": "Bumi",
-    "url": "https://michaelbumann.com",
-    "accounts": [
-      {
-        "site": "github.com",
-        "username": "bumi",
-        "uid": 318,
-        "url": "https://github.com/bumi/"
-      },
-    ]
-  },
-  {
-    "@context": "https://schema.kosmos.org",
-    "@type": "Contribution",
-    "contributor": {
-      "ipfs": "QmQ2ZZS2bXgneQfKtVTVxe6dV7pcJuXnTeZJQtoVUFsAtJ"
-    },
-    "kind": "dev",
-    "description": "hacking hacking on kredits",
-    "url": "https://github.com/67P/kredits-web/pull/11",
-    "details": {}
-  }
-
-]
-
-module.exports = { contractCalls, ipfsContent };
+let contractCalls = [
+  ['Contributor', 'add', [{ account: '0x7e8f313c56f809188313aa274fa67ee58c31515d', name: 'bumi', isCore: true, kind: 'preson', url: '', github_username: 'bumi', github_uid: 318, wiki_username: 'bumi' }, {gasLimit: 200000}]],
+  ['Contributor', 'add', [{ account: '0xa502eb4021f3b9ab62f75b57a94e1cfbf81fd827', name: 'raucau', isCore: true, kind: 'person', url: '', github_username: 'skddc', github_uid: 842, wiki_username: 'raucau' }, {gasLimit: 200000}]],
+  ['Operator', 'addProposal', [{ contributorId: 2, amount: 42, kind: 'code', description: 'runs the seeds', url: '' }, {gasLimit: 350000}]],
+  ['Operator', 'addProposal', [{ contributorId: 3, amount: 23, kind: 'code', description: 'runs the seeds', url: '' }, {gasLimit: 350000}]],
+  ['Operator', 'addProposal', [{contributorId: 3, amount: 100, kind: 'code', description: 'hacks on kredits', url: '' }, {gasLimit: 350000}]],
+  ['Operator', 'vote', ['1', {gasLimit: 250000}]]
+];
+module.exports = { contractCalls };
--- scripts/add-contributor.js
+++ scripts/add-contributor.js
@@ -1,45 +1,50 @@
 const Registry = artifacts.require('./Registry.sol');
-const Operator = artifacts.require('./Operator.sol');
-const Contributors = artifacts.require('./Contributors.sol');
 const promptly = require('promptly');
-
 const bs58 = require('bs58');
 
-function getBytes32FromMultiash(multihash) {
-  const decoded = bs58.decode(multihash);
+const ethers = require('ethers');
+const Kredits = require('../lib/kredits');
 
-  return {
-    digest: `0x${decoded.slice(2).toString('hex')}`,
-    hashFunction: decoded[0],
-    size: decoded[1],
-  };
+async function prompt(message, options) {
+  if (!options) {
+    options = {default: ''}
+  }
+  return await promptly.prompt(message, options);
 }
 
 module.exports = function(callback) {
   Registry.deployed().then(async (registry) => {
-    var operatorAddress = await registry.getProxyFor('Operator');
-    var contributorsAddress = await registry.getProxyFor('Contributors');
 
-    var operator = await Operator.at(operatorAddress);
-    var contributors = await Contributors.at(contributorsAddress);
+    const networkId = parseInt(web3.version.network);
+    const provider = new ethers.providers.Web3Provider(
+      web3.currentProvider, { chainId: networkId }
+    );
+    const kredits = await Kredits.setup(provider, provider.getSigner());
 
-    let contributorToAddAddress = await promptly.prompt('Contributor address: ');
-    let ipfsHash = await promptly.prompt('IPFS hash (blank for default): ', { default: 'QmQNA1hhVyL1Vm6HiRxXe9xmc6LUMBDyiNMVgsjThtyevs' });
+    console.log(`Using contributors at: ${kredits.Contributor.contract.address}`);
 
-    let contributorMultihash = getBytes32FromMultiash(ipfsHash);
-    let isCore = true;
-    let contributorResult = await contributors.addContributor(contributorToAddAddress, contributorMultihash.digest, contributorMultihash.hashFunction, contributorMultihash.size, isCore);
-    console.log('Contributor added, tx: ', contributorResult.tx);
+    let contributorAttributes = {
+      account: await prompt('Contributor address: ', {}),
+      name: await prompt('Name: '),
+      isCore: await prompt('core? y/n') === 'y',
+      kind: await prompt('Kind (default person): ', {default: 'person'}),
+      url: await prompt('URL: '),
+      github_username: await prompt('GitHub username: '),
+      github_uid: await prompt('GitHub UID: '),
+      wiki_username: await prompt('Wiki username: '),
+    };
 
-    let contributorId = await contributors.getContributorIdByAddress(contributorToAddAddress);
-    let proposalMultihash = getBytes32FromMultiash('QmQNA1hhVyL1Vm6HiRxXe9xmc6LUMBDyiNMVgsjThtyevs');
-    let proposalResult = await operator.addProposal(contributorId, 23, proposalMultihash.digest, proposalMultihash.hashFunction, proposalMultihash.size);
-    console.log('Proposal added, tx: ', proposalResult.tx);
+    console.log("\nAdding contributor:");
+    console.log(contributorAttributes);
 
-    let proposalId = await operator.proposalsCount();
-    let votingResult = await operator.vote(proposalId.toNumber()-1);
-    console.log('Voted for proposal', proposalId.toString(), votingResult.tx);
+    kredits.Contributor.add(contributorAttributes, {gasLimit: 250000}).then((result) => {
+      console.log("\n\nResult:");
+      console.log(result);
+      callback();
+    }).catch((error) => {
+      console.log('Failed to create contributor');
+      callback(error);
+    });
 
-    callback();
   });
 }
