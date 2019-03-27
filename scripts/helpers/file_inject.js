b6f34ac9a5795fdc0960acd3b330ac3cdb3ecabd
Michael Bumann <hello@michaelbumann.com>
2019-03-27 21:58:19 +0100

    Pretty print JSON for contract addresses
    This makes it easier to read the file and a git diff is more useful

--- scripts/helpers/file_inject.js
+++ scripts/helpers/file_inject.js
@@ -4,5 +4,5 @@ const fs = require('fs');
 module.exports = function (file, networkId, data) {
   let content = JSON.parse(fs.readFileSync(file));
   content[networkId] = data;
-  fs.writeFileSync(file, JSON.stringify(content));
+  fs.writeFileSync(file, JSON.stringify(content, null, 2));
 }
