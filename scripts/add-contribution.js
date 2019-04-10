130e2a7797f6763c2b52ea2faba02f1c57919557
Sebastian Kippe <sebastian@kip.pe>
2019-04-10 18:37:02 +0200

    Validate contribution docs against schema
    Currently requires an open PR branch for the schemas, which is adding
    date and time for contributions.
    
    refs #30

--- scripts/add-contribution.js
+++ scripts/add-contribution.js
@@ -1,4 +1,5 @@
 const promptly = require('promptly');
+const { inspect } = require('util');
 
 const initKredits = require('./helpers/init_kredits.js');
 
@@ -26,23 +27,32 @@ module.exports = async function(callback) {
 
   console.log(`Creating a contribution for contributor account ${contributorAccount} ID: ${contributorId}`);
 
+  [ dateNow, timeNow ] = (new Date()).toISOString().split('T');
+
   let contributionAttributes = {
     contributorId,
+    date: dateNow,
+    time: timeNow,
     amount: await promptly.prompt('Amount: '),
     description: await promptly.prompt('Description: '),
     kind: await promptly.prompt('Kind: ', { default: 'dev' }),
     url: await promptly.prompt('URL: ', { default: '' })
   }
 
+  const contributorData = await kredits.Contributor.getById(contributorId);
+  contributionAttributes.contributorIpfsHash = contributorData.ipfsHash;
+
   console.log("\nAdding contribution:");
   console.log(contributionAttributes);
 
-  kredits.Contribution.addContribution(contributionAttributes, { gasLimit: 300000 }).then((result) => {
-    console.log("\n\nResult:");
-    console.log(result);
-    callback();
-  }).catch((error) => {
-    console.log('Failed to create contribution');
-    callback(error);
-  });
+  kredits.Contribution.addContribution(contributionAttributes, { gasLimit: 300000 })
+    .then(result => {
+      console.log("\n\nResult:");
+      console.log(result);
+      callback();
+    })
+    .catch(error => {
+      console.log('Failed to create contribution');
+      callback(inspect(error));
+    });
 }
