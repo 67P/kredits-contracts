a95e1ec027f3660c102bcd9cc38fb537f077d416
Haythem Sellami <sellamihaythem96@gmail.com>
2019-04-20 14:44:42 +0100

    refactor token tests

--- apps/token/test/app.js
+++
@@ -1,5 +0,0 @@
-const Token = artifacts.require('Token.sol')
-
-contract('Token', (accounts) => {
-  it('should be tested')
-})
--- apps/token/test/token.js
+++ apps/token/test/token.js
@@ -7,7 +7,7 @@ const { assertRevert } = require('@aragon/test-helpers/assertThrow');
 
 const ZERO_ADDR = '0x0000000000000000000000000000000000000000';
 
-contract('Coin app', (accounts) => {
+contract('Token app', (accounts) => {
     let kernelBase, aclBase, daoFactory, dao, acl, token;
   
     const root = accounts[0];
