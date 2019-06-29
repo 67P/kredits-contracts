0320fa59f63c2de3e3219c6d28a1a1a6757acd2a
Haythem Sellami <sellamihaythem96@gmail.com>
2019-06-29 14:29:23 +0100

    init vault app

---
+++ apps/vault/contracts/Vault.sol
@@ -0,0 +1,74 @@
+pragma solidity ^0.4.24;
+
+import "@aragon/os/contracts/apps/AragonApp.sol";
+import "@aragon/os/contracts/common/DepositableStorage.sol";
+import "@aragon/os/contracts/common/EtherTokenConstant.sol";
+import "@aragon/os/contracts/common/SafeERC20.sol";
+import "@aragon/os/contracts/lib/token/ERC20.sol";
+
+contract Vault is EtherTokenConstant, AragonApp, DepositableStorage {
+    using SafeERC20 for ERC20;
+
+    string private constant ERROR_NOT_DEPOSITABLE = "VAULT_NOT_DEPOSITABLE";
+    string private constant ERROR_DEPOSIT_VALUE_ZERO = "VAULT_DEPOSIT_VALUE_ZERO";
+    string private constant ERROR_VALUE_MISMATCH = "VAULT_VALUE_MISMATCH";
+    string private constant ERROR_TOKEN_TRANSFER_FROM_REVERTED = "VAULT_TOKEN_TRANSFER_FROM_REVERT";
+
+    event VaultDeposit(address indexed token, address indexed sender, uint256 amount);
+
+    function () external payable isInitialized {
+        _deposit(ETH, msg.value);
+    }
+
+    /**
+    * @notice Initialize Vault app
+    * @dev As an AragonApp it needs to be initialized in order for roles (`auth` and `authP`) to work
+    */
+    function initialize() external onlyInit {
+        initialized();
+        setDepositable(true);
+    }
+
+    /**
+    * @notice Deposit `_value` `_token` to the vault
+    * @param _token Address of the token being transferred
+    * @param _value Amount of tokens being transferred
+    */
+    function deposit(address _token, uint256 _value) external payable isInitialized {
+        _deposit(_token, _value);
+    }
+
+    function balance(address _token) public view returns (uint256) {
+        if (_token == ETH) {
+            return address(this).balance;
+        } else {
+            return ERC20(_token).staticBalanceOf(address(this));
+        }
+    }
+
+    /**
+    * @dev Disable recovery escape hatch, as it could be used
+    *      maliciously to transfer funds away from the vault
+    */
+    function allowRecoverability(address) public view returns (bool) {
+        return false;
+    }
+
+    function _deposit(address _token, uint256 _value) internal {
+        require(isDepositable(), ERROR_NOT_DEPOSITABLE);
+        require(_value > 0, ERROR_DEPOSIT_VALUE_ZERO);
+
+        if (_token == ETH) {
+            // Deposit is implicit in this case
+            require(msg.value == _value, ERROR_VALUE_MISMATCH);
+        } else {
+            require(
+                ERC20(_token).safeTransferFrom(msg.sender, address(this), _value),
+                ERROR_TOKEN_TRANSFER_FROM_REVERTED
+            );
+        }
+
+        emit VaultDeposit(_token, msg.sender, _value);
+    }
+
+}
