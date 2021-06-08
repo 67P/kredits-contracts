f5ca44d7793e85be3aca8a569f44312ad86894ee
Michael Bumann <hello@michaelbumann.com>
2021-06-08 23:30:27 +0200

    ByeBye apps

--- apps/contribution/test/app.js
+++
@@ -1,5 +0,0 @@
-// const Contribution = artifacts.require('Contribution.sol');
-
-contract('Contribution', (_accounts) => {
-  it('should be tested');
-});
--- apps/proposal/test/app.js
+++
@@ -1,5 +0,0 @@
-// const Proposal = artifacts.require('Proposal.sol');
-
-contract('Proposal', (_accounts) => {
-  it('should be tested');
-});
