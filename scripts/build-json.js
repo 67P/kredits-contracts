ba52e61d50cc63ba4a7f088ee797e9e865d5a65d
Michael Bumann <hello@michaelbumann.com>
2022-05-19 18:06:56 +0200

    Load contract APIs from hardhat artifacts

--- scripts/build-json.js
+++ scripts/build-json.js
@@ -1,24 +1,18 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
-const contractsPath = path.join(__dirname, '..', 'build', 'contracts');
-const libPath = path.join(__dirname, '..', 'lib');
-const abisPath = path.join(libPath, 'abis');
+const contractsPath = path.join(__dirname, "..", "artifacts", "contracts");
+const libPath = path.join(__dirname, "..", "lib");
+const abisPath = path.join(libPath, "abis");
 
-const files = [
-  'Contributor',
-  'Contribution',
-  'Kernel',
-  'Proposal',
-  'Token',
-  'Reimbursement',
-  'ACL'
-];
+const files = ["Contributor", "Contribution", "Token", "Reimbursement"];
 
 files.forEach((fileName) => {
-  let file = require(`${contractsPath}/${fileName}.json`);
+  let file = require(`${contractsPath}/${fileName}.sol/${fileName}.json`);
   let abiFile = path.join(abisPath, `${fileName}.json`);
   fs.writeFileSync(abiFile, JSON.stringify(file.abi));
 });
 
-console.log("Don't forget to reaload the JSON files from your application; i.e. restart kredits-web");
+console.log(
+  "Don't forget to reaload the JSON files from your application; i.e. restart kredits-web"
+);
