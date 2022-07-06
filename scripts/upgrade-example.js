bcee608920760d95362b855eb658d2ca3b3eeba7
Michael Bumann <hello@michaelbumann.com>
2022-07-06 18:07:38 +0200

    Typo

--- scripts/upgrade-example.js
+++ scripts/upgrade-example.js
@@ -9,7 +9,7 @@ async function main() {
   const ContributorV2 = await ethers.getContractFactory("Contributor");
   const contributor = await upgrades.upgradeProxy(kredits.Contributor.address, ContributorV2);
   console.log("Contributor upgraded");
-  console.log(`Contirbutor address: ${contributor.address}`);
+  console.log(`Contributor address: ${contributor.address}`);
 
   await contributor.deployTransaction.wait();
 
