e99184b83f32bb24276a80754d67fd10006a6bab
Râu Cao <raucao@kip.pe>
2022-08-25 11:51:31 +0200

    Make test complete, add FIXME note

--- test/contracts/Contributor.js
+++ test/contracts/Contributor.js
@@ -97,6 +97,8 @@ describe("Contributor contract", async function () {
       c = await Contributor.getContributorById(2);
       // TODO Are we required to use Wei for ERC20 tokens?
       expect(c['balance'].toString()).to.equal("6500000000000000000000");
+      // FIXME This is also a BigNumber (uint256), but stored at Wei reolution
+      expect(c['kreditsWithdrawn'].toString()).to.equal('6500');
     });
 
     it("requires the withdrawable amount to be larger than 0", async function () {
