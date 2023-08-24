30490ce393fbee51207fa9394ff71940ebedb9e2
Râu Cao <raucao@kip.pe>
2023-08-24 14:52:38 +0200

    "Fix" default network name in hardhat config
    Some scripts require "hardhat" while others require "localhost". But we
    can fix whatever doesn't work directly in the package config.

--- hardhat.config.js
+++ hardhat.config.js
@@ -62,7 +62,7 @@ task("create-wallet", "Creates a new wallet json", async () => {
  */
 module.exports = {
   solidity: "0.8.2",
-  defaultNetwork: "hardhat",
+  defaultNetwork: "localhost",
   networks: {
     hardhat: {
       chainId: 1337,
